251fb32a7406d8b47a086b3582aae7f6ed663086
andra <andraresmi@gmail.com>
2020-09-10 16:28:15 +0700

    andra202009096

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 
 
 //use App\Transaksi;
+use App\Tiket;
+use App\Tiketdetail;
+use App\Histori;
+use App\Nextnumber;
+use App\Layanan;
+use App\Transaksiot;
+use App\Service;
+use App\Subservice;
 
 use DB;
 use Auth;
@@ -30,7 +38,94 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        return view('home');
+    {    
+        if(session('infoUser')['LEVEL'] == 'admin')
+        {
+            #$datas = Tiket::with(['layanan', 'service', 'subService', 'tiketDetail'])->get();
+            $datas = DB::table('tiket as a')
+                ->select(
+                    'a.tiketId',
+                    'a.kode_tiket',          
+                    'a.comp',          
+                    'a.unit',          
+                    'a.nikUser',
+                    'g.name',
+                    'a.layananId',         
+                    'c.nama_layanan',          
+                    'a.serviceId',             
+                    'd.ServiceName',          
+                    'a.subServiceId',            
+                    'e.ServiceSubName',           
+                    'a.tiketKeterangan',          
+                    'a.tiketApprove',
+                    'a.created_at',          
+                    'a.tiketTglApprove',
+					'a.tiketTglApproveService',
+                    'b.tglMulaiMengerjakan',
+                    'b.tglSelesaiMengerjakan',
+                    'a.tiketNikAtasan',          
+                    'a.tiketPrioritas',          
+                    'a.tiketStatus',          
+                    'a.created_at',
+                    'b.nikTeknisi',
+                    'f.progresProsen'
+                )
+                ->leftjoin('tiket_detail as b', 'b.tiketId', '=', 'a.tiketId')
+                ->leftjoin('m_layanan as c', 'c.id', '=', 'a.layananId')
+                ->leftjoin('ticket_service as d', 'd.id', '=', 'a.serviceId')
+                ->leftjoin('ticket_service_sub as e', 'e.id', '=', 'a.subServiceId')
+                ->leftjoin('m_progres as f', 'f.progresId', '=', 'b.progresId')
+                ->leftjoin('users as g', 'g.username', '=', 'a.nikUser')
+                ->orderBy('a.tiketStatus', 'asc')
+                ->orderBy('a.kode_tiket', 'asc')
+                ->get();
+        } else {
+            $datas = DB::table('tiket as a')
+                ->select(
+                    'a.tiketId',
+                    'a.kode_tiket',          
+                    'a.comp',          
+                    'a.unit',          
+                    'a.nikUser',
+                    'g.name',
+                    'a.layananId',         
+                    'c.nama_layanan',          
+                    'a.serviceId',             
+                    'd.ServiceName',          
+                    'a.subServiceId',            
+                    'e.ServiceSubName',           
+                    'a.tiketKeterangan',          
+                    'a.tiketApprove',
+                    'a.created_at',          
+                    'a.tiketTglApprove',
+					'a.tiketTglApproveService',
+                    'b.tglMulaiMengerjakan',
+                    'b.tglSelesaiMengerjakan',
+                    'a.tiketNikAtasan',          
+                    'a.tiketPrioritas',          
+                    'a.tiketStatus',          
+                    'a.created_at',
+                    'b.nikTeknisi',
+                    'f.progresProsen'
+                )
+                ->leftjoin('tiket_detail as b', 'b.tiketId', '=', 'a.tiketId')
+                ->leftjoin('m_layanan as c', 'c.id', '=', 'a.layananId')
+                ->leftjoin('ticket_service as d', 'd.id', '=', 'a.serviceId')
+                ->leftjoin('ticket_service_sub as e', 'e.id', '=', 'a.subServiceId')
+                ->leftjoin('m_progres as f', 'f.progresId', '=', 'b.progresId')
+                ->leftjoin('users as g', 'g.username', '=', 'a.nikUser')
+                ->where(['a.nikUser' => session('infoUser')['NIK']])
+                ->orderBy('a.tiketStatus', 'asc')
+                ->orderBy('a.kode_tiket', 'asc')
+                ->get();
+        }
+        //dd($datas);
+        return view('home', ['datas'=>$datas, 'kode'=>'', 'pesan'=>'']);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,14 +93,91 @@
               </div>
             </div>
 </div>
-<div class="row" >
+<div class="row">
+
+  
+    <div class="col-lg-12">
+                  @if (Session::has('message'))
+                  <div class="alert alert-{{ Session::get('message_type') }}" id="waktu2" style="margin-top:10px;">{{ Session::get('message') }}</div>
+                  @endif
+                  </div>
+</div>
+<div class="row" style="margin-top: 20px;">
 <div class="col-lg-12 grid-margin stretch-card">
               <div class="card">
 
                 <div class="card-body">
-                  <h4 class="card-title">Data yang masih Open Ticket</h4>
+                  <h4 class="card-title">Status Open Ticket sampai dengan hari ini</h4>
                   
-                                  </div>
+                  <div class="table-responsive">
+                    <table class="table table-striped" id="table">
+                      <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nomor</th>
+                            <th>Layanan</th>
+                            <th>Service</th>
+                            <th>Subservice</th>
+                            <th>Keterangan</th>
+                            <th>Tgl Open</th>
+                            <th>Tgl Approve Atasan</th>
+                            <th>Tgl Approve Atasan IT</th>
+                            <th>Tgl Mulai</th>
+                            <th>Tgl Selesai</th>
+                            <th>User</th>
+                            <th>Progress (%)</th>
+                            <th>Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      @foreach($datas as $data)
+
+                        <tr>
+                            <td class="py-1">{{$loop->iteration}}</td>
+                            <td class="py-1">{{$data->kode_tiket}}</td>
+                            <td class="py-1">{{$data->nama_layanan}}</td>
+                            <td class="py-1">{{$data->ServiceName}}</td>
+                            <td>{{$data->ServiceSubName}}</td>
+                            <td>{{$data->tiketKeterangan}}</td>
+                            <td>{{$data->created_at}}</td>
+                            <td>{{$data->tiketTglApprove}}</td>
+                            <td>{{$data->tiketTglApproveService}}</td>
+                            <td> {{$data->tglMulaiMengerjakan}}   </td>
+                            <td> {{$data->tglSelesaiMengerjakan}}   </td>
+                            <td>  {{$data->name}}      </td>
+                            <td>  {{$data->progresProsen}}% </td>
+                            <td>
+                            @if($data->tiketStatus == '1')
+                                <label class="badge badge-warning">open</label>
+                            @elseif($data->tiketStatus == '2')
+                                <label class="badge badge-warning">Diapprove Atasan Unit</label>
+                            @elseif($data->tiketStatus == '3')
+                                <label class="badge badge-danger">Ditolak Atasan Unit</label>
+                            @elseif($data->tiketStatus == '4')
+                                <label class="badge badge-success">Disetujui</label>
+                            @elseif($data->tiketStatus == '5')
+                                <label class="badge badge-danger">Ditolak</label>
+                            @elseif($data->tiketStatus == '6')
+                                <label class="badge badge-info">Dikerjakan</label>
+                            @elseif($data->tiketStatus == '7')
+                                <label class="badge badge-primary">Selesai</label>
+                            @elseif($data->tiketStatus == '8')
+                                <label class="badge badge-dark">Close</label>
+                            @elseif($data->tiketStatus == '9')
+                                <label class="badge badge-warning">Pending</label>
+                            @elseif($data->tiketStatus == '10')
+                                <label class="badge badge-danger">Cancle</label>
+                            @endif
+    
+                            </td>
+                        </tr>
+
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                  {{--  {!! $datas->links() !!} --}}
+                </div>
               </div>
             </div>
           </div>
