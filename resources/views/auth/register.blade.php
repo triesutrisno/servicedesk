a96a98f7b79b737e643665ed67d5b8e92995384f
Triesutrisno <triesutrisno@gmail.com>
2020-09-14 15:04:15 +0700

    ServicedeskSilogGroup #2020-09-14 15:04

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -73,18 +73,6 @@ var check = function() {
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
                         <div class="form-group">
                             <label for="email" class="col-md-4 control-label">Gambar</label>
                             <div class="col-md-6">
@@ -101,31 +89,7 @@ var check = function() {
                                 <option value=""></option>
                                 <option value="admin">Admin</option>
                                 <option value="user">User</option>
-                                <option value="atasan_bidang">Atasan Bidang</option>
-                                <option value="atasan_it">Atasan IT</option>
-                                <option value="petugas_it">Petugas IT</option>
-                                <option value="client">Client</option>
                             </select>
-                            </div>
-                        </div>
-
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" onkeyup='check();' name="password" required>
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="confirm_password" type="password" onkeyup="check()" class="form-control" name="password_confirmation" required>
-                                <span id='message'></span>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary" id="submit">
