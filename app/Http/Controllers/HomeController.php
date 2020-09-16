3dd8e72d9c596562740ed66d29ec6f48bf2cc30d
andra <andraresmi@gmail.com>
2020-09-16 14:54:05 +0700

    andra20200916

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,8 +39,7 @@ class HomeController extends Controller
      */
     public function index()
     {    
-        if(session('infoUser')['LEVEL'] == 'admin')
-        {
+       
             #$datas = Tiket::with(['layanan', 'service', 'subService', 'tiketDetail'])->get();
             $datas = DB::table('tiket as a')
                 ->select(
@@ -79,46 +78,6 @@ class HomeController extends Controller
                 ->orderBy('a.tiketStatus', 'asc')
                 ->orderBy('a.kode_tiket', 'asc')
                 ->get();
-        } else {
-            $datas = DB::table('tiket as a')
-                ->select(
-                    'a.tiketId',
-                    'a.kode_tiket',          
-                    'a.comp',          
-                    'a.unit',          
-                    'a.nikUser',
-                    'g.name',
-                    'a.layananId',         
-                    'c.nama_layanan',          
-                    'a.serviceId',             
-                    'd.ServiceName',          
-                    'a.subServiceId',            
-                    'e.ServiceSubName',           
-                    'a.tiketKeterangan',          
-                    'a.tiketApprove',
-                    'a.created_at',          
-                    'a.tiketTglApprove',
-					'a.tiketTglApproveService',
-                    'b.tglMulaiMengerjakan',
-                    'b.tglSelesaiMengerjakan',
-                    'a.tiketNikAtasan',          
-                    'a.tiketPrioritas',          
-                    'a.tiketStatus',          
-                    'a.created_at',
-                    'b.nikTeknisi',
-                    'f.progresProsen'
-                )
-                ->leftjoin('tiket_detail as b', 'b.tiketId', '=', 'a.tiketId')
-                ->leftjoin('m_layanan as c', 'c.id', '=', 'a.layananId')
-                ->leftjoin('ticket_service as d', 'd.id', '=', 'a.serviceId')
-                ->leftjoin('ticket_service_sub as e', 'e.id', '=', 'a.subServiceId')
-                ->leftjoin('m_progres as f', 'f.progresId', '=', 'b.progresId')
-                ->leftjoin('users as g', 'g.username', '=', 'a.nikUser')
-                ->where(['a.nikUser' => session('infoUser')['NIK']])
-                ->orderBy('a.tiketStatus', 'asc')
-                ->orderBy('a.kode_tiket', 'asc')
-                ->get();
-        }
         //dd($datas);
         return view('home', ['datas'=>$datas, 'kode'=>'', 'pesan'=>'']);
     }
