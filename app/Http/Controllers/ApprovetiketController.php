e85fc8d6ee8d40b7c0780cab8293007cbb559517
Triesutrisno <triesutrisno@gmail.com>
2020-10-01 10:08:32 +0700

    ServicedeskSilogGroup #2020-10-01 10:08

--- app/Http/Controllers/ApprovetiketController.php
+++ app/Http/Controllers/ApprovetiketController.php
@@ -104,7 +104,7 @@ class ApprovetiketController extends Controller
             $isiEmail.="<tr>";
             $isiEmail.="<td>UserBy</td>";
             $isiEmail.="<td>:</td>";
-            $isiEmail.="<td>".$tiket[0]['userBy'][0]['name']."</td>";
+            $isiEmail.="<td>".$tiket[0]['userBy']['name']."</td>";
             $isiEmail.="</tr>";            
             $isiEmail.="</table><br />";
             $isiEmail.="Silakan akses tiket.silog.co.id dan gunakan user dan password anda untuk login ke aplikasi tersebut. <br />";
@@ -139,7 +139,7 @@ class ApprovetiketController extends Controller
                 $isiTelegram.="Service : ".$tiket[0]['service'][0]['ServiceName']." \n";
                 $isiTelegram.="Subservice : ".$tiket[0]['subService'][0]['ServiceSubName']." \n";
                 $isiTelegram.="Keterangan : ".$tiket[0]['tiketKeterangan']." \n";
-                $isiTelegram.="UserBy : ".$tiket[0]['userBy'][0]['name']." \n\n";
+                $isiTelegram.="UserBy : ".$tiket[0]['userBy']['name']." \n\n";
                 $isiTelegram.="Silakan akses tiket.silog.co.id dan gunakan user dan password anda untuk login ke aplikasi tersebut. \n";
 
                 $urle2 = env('API_BASE_URL')."/sendTelegram.php";
