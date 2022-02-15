3a58d858f4fa11ccffbd73b2cd77483a3fa9131d
triesutrisno <triesutrisno@SI-NB-SUTRIS.silog.co.id>
2022-02-15 08:57:36 +0700

    tiketSilog 2022-02-15 08:57

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -44,7 +44,7 @@
     </li>
     @endif   
 
-    @if(session('infoUser')['ESELON'] <= '40')
+    @if(session('infoUser')['ESELON'] <= '40' || session('infoUser')['ESELON']=='DO')
       <li class="nav-item">
           <a class="nav-link" href="{{url('approvetiket')}}">
             <i class="menu-icon fa fa-handshake-o"></i>
