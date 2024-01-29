e78e678c291f791b1b134ec88d8bff21bd6833b8
tomycahya2 <tomycahya2@gmail.com>
2024-01-29 16:25:41 +0700

    update email

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -492,7 +492,7 @@ class HomeController extends Controller
                                 'subjectEmail' => 'Info Permintaan Tiket',
                                 'isiEmail' => addslashes($isiEmail),
                                 'status' => 'outbox',
-                                'password' => 'Veteran1974!@Gsk',
+                                'password' => 'Veteran1974!@Gs',
                                 'contentEmail' => '0',
                                 'sistem' => 'tiketSilog',
                             ]);
@@ -665,7 +665,7 @@ class HomeController extends Controller
                             'subjectEmail' => 'Info Pengerjaan Tiket',
                             'isiEmail' => addslashes($isiEmail),
                             'status' => 'outbox',
-                            'password' => 'Veteran1974!@Gsk',
+                            'password' => 'Veteran1974!@Gs',
                             'contentEmail' => '0',
                             'sistem' => 'tiketSilog',
                         ]);
@@ -834,7 +834,7 @@ class HomeController extends Controller
                                 'subjectEmail' => 'Info Permintaan Tiket',
                                 'isiEmail' => addslashes($isiEmail),
                                 'status' => 'outbox',
-                                'password' => 'Veteran1974!@Gsk',
+                                'password' => 'Veteran1974!@Gs',
                                 'contentEmail' => '0',
                                 'sistem' => 'tiketSilog',
                             ]);
